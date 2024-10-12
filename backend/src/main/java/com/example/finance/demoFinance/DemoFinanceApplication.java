d1f38e9b1bf706de7742793f26f5ceb4c9f2d658
SethBaugh <sethbaugh70@gmail.com>
2024-10-12 17:17:07 -0400

    adding springboot

---
+++ backend/src/main/java/com/example/finance/demoFinance/DemoFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.finance.demoFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFinanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/finance/demoFinance/DemoFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finance.demoFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
